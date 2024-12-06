064b95706cfdfaa6000ce4fae8f3f5d3600925b1
Халтаев Никита Сергеевич <21073@iat.iat>
2024-12-06 17:01:25 +0800

    data picker fix | utits installed"

--- resources/js/Pages/ProjectCreate.vue
+++ resources/js/Pages/ProjectCreate.vue
@@ -1,38 +1,39 @@
 <script setup lang="ts">
-    import { Head, Link } from '@inertiajs/vue3';
-    import Footer from '@/Components/main/Footer.vue'
-    import Header from '@/Components/main/Header.vue'
+import { Head, Link } from '@inertiajs/vue3';
+import Footer from '@/Components/main/Footer.vue'
+import Header from '@/Components/main/Header.vue'
 
-    const props = defineProps<{
-        canLogin?: boolean;
-        canRegister?: boolean;
-    }>();
+const props = defineProps<{
+    canLogin?: boolean;
+    canRegister?: boolean;
+}>();
 
-    import type { DateRange } from 'radix-vue'
-    import { Button } from '@/Components/ui/button'
+import type { DateRange } from 'radix-vue'
+import { Button } from '@/Components/ui/button'
 
-    import { Popover, PopoverContent, PopoverTrigger } from '@/Components/ui/popover'
-    import { RangeCalendar } from '@/Components/ui/range-calendar'
-    import { cn } from '@/utils'
-    import {
-        CalendarDate,
-        DateFormatter,
-        getLocalTimeZone,
-    } from '@internationalized/date'
-    import { CalendarIcon } from '@radix-icons/vue'
-    import { type Ref, ref } from 'vue'
+import { Popover, PopoverContent, PopoverTrigger } from '@/Components/ui/popover'
+import { RangeCalendar } from '@/Components/ui/range-calendar'
+import { cn } from '@/Components/utils/utils'
+import {
+    CalendarDate,
+    DateFormatter,
+    getLocalTimeZone,
+} from '@internationalized/date'
+import { CalendarIcon } from '@radix-icons/vue'
+import { type Ref, ref } from 'vue'
 
-    const df = new DateFormatter('en-US', {
-        dateStyle: 'medium',
-    })
+const df = new DateFormatter('en-US', {
+    dateStyle: 'medium',
+})
 
-    const value = ref({
-        start: new CalendarDate(2022, 1, 20),
-        end: new CalendarDate(2022, 1, 20).add({ days: 20 }),
-    }) as Ref<DateRange>
+const value = ref({
+    start: new CalendarDate(2022, 1, 20),
+    end: new CalendarDate(2022, 1, 20).add({ days: 20 }),
+}) as Ref<DateRange>
 </script>
 
 <template>
+
     <Head title="Создать проект" />
 
     <Header />
@@ -42,33 +43,33 @@
             <h1 class="text-white text-center font-bold text-4xl mb-10">
                 Создать новый проект
             </h1>
-            
+
             <form class="flex flex-col gap-5">
                 <div class="flex flex-col gap-2">
                     <label class="text-white">Название проекта</label>
-                    <input type="text" class="text-white w-full h-10 px-4 border border-white/20 bg-transparent focus:border-white/60 focus:ring-2 focus:ring-white/60 focus:rounded focus:shadow-md focus:shadow-white/40">
+                    <input type="text"
+                        class="text-white w-full h-10 px-4 border border-white/20 bg-transparent focus:border-white/60 focus:ring-2 focus:ring-white/60 focus:rounded focus:shadow-md focus:shadow-white/40">
                 </div>
                 <div class="flex flex-col gap-2">
                     <label class="text-white">Описание проекта</label>
-                    <textarea type="text" class="text-white w-full h-24 px-4 py-2 border border-white/20 bg-transparent focus:border-white/60 focus:ring-2 focus:ring-white/60 focus:rounded focus:shadow-md focus:shadow-white/40"></textarea>
+                    <textarea type="text"
+                        class="text-white w-full h-24 px-4 py-2 border border-white/20 bg-transparent focus:border-white/60 focus:ring-2 focus:ring-white/60 focus:rounded focus:shadow-md focus:shadow-white/40"></textarea>
                 </div>
                 <div class="flex flex-col gap-2">
                     <label class="text-white">Дедлайн</label>
                     <Popover>
                         <PopoverTrigger as-child>
-                            <Button
-                                variant="outline"
-                                :class="cn(
-                                    'w-[280px] justify-start text-left font-normal',
-                                    !value && 'text-muted-foreground',
-                                )"
-                            >
+                            <Button variant="outline" :class="cn(
+                                'w-[280px] justify-start text-left font-normal',
+                                !value && 'text-muted-foreground',
+                            )">
                                 <CalendarIcon class="mr-2 h-4 w-4" />
                                 <template v-if="value.start">
                                     <template v-if="value.end">
-                                        {{ df.format(value.start.toDate(getLocalTimeZone())) }} - {{ df.format(value.end.toDate(getLocalTimeZone())) }}
+                                        {{ df.format(value.start.toDate(getLocalTimeZone())) }} - {{
+                                            df.format(value.end.toDate(getLocalTimeZone())) }}
                                     </template>
-                            
+
                                     <template v-else>
                                         {{ df.format(value.start.toDate(getLocalTimeZone())) }}
                                     </template>
@@ -79,7 +80,8 @@
                             </Button>
                         </PopoverTrigger>
                         <PopoverContent class="w-auto p-0">
-                            <RangeCalendar v-model="value" initial-focus :number-of-months="2" @update:start-value="(startDate) => value.start = startDate" />
+                            <RangeCalendar v-model="value" initial-focus :number-of-months="2"
+                                @update:start-value="(startDate) => value.start = startDate" />
                         </PopoverContent>
                     </Popover>
                 </div>
@@ -87,8 +89,5 @@
         </div>
     </main>
 
-    <Footer
-        :canLogin="canLogin"
-        :canRegister="canRegister"
-    />
+    <Footer :canLogin="canLogin" :canRegister="canRegister" />
 </template>
